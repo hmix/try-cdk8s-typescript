78346907cb0366f8a814da92b9935c739c2e91b1
hmix <hmi@foo42.info>
2020-10-04 13:08:51 +0200

    add Helloworld deployment and LB service

--- main.ts
+++ main.ts
@@ -1,11 +1,43 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
+// imported constructs
+import { Deployment, Service, IntOrString } from './imports/k8s';
 
 export class MyChart extends Chart {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     // define resources here
+    const label = { app: 'try_cdk8s-typescript'};
+
+    new Service(this, 'myservice', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [{ port: 80, targetPort: IntOrString.fromNumber(8080)}],
+        selector: label
+      }
+    });
+
+    new Deployment(this, 'mydeployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.7',
+                ports: [{ containerPort: 8080 }]
+              }
+            ]
+          }
+        }
+      }
+    });
 
   }
 }
